0ac2459412c0d0202b18f9ea207b7d16bf254403
Partrester12 <tapsa.n@icloud.com>
2022-12-28 11:48:05 +0200

    Removed unnecessary tests

--- src/test/java/org/tn/ViolatorsResourceIT.java
+++
@@ -1,8 +0,0 @@
-package org.tn;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class ViolatorsResourceIT extends ViolatorsResourceTest {
-    // Execute the same tests but in packaged mode.
-}
